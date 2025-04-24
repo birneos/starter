1743ac2048da4b4ddc189b0fbaa58b93933854b5
Marco Linke <birneos@gmx.net>
2025-04-24 21:40:19 +0200

    Benutze react/icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from 'react'
+import people from './data.js'
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from 'react-icons/fa'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
-};
-export default App;
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+
+  console.log(name)
+
+  return (
+    <main>
+      <article className="review">
+        <div className="div img-container">
+          <img className="person-img" alt={name} src={image} />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+      <FaChevronCircleLeft />
+      <FaChevronCircleRight />
+    </main>
+  )
+}
+export default App
